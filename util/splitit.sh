ba5c04476de6c03239c47c9538b7fd2cad7e27ae
Kostas Peletidis <kpeletidis@gmail.com>
2023-06-10 16:40:21 +0200

    Use temporary file to store thread number info

--- util/splitit.sh
+++ util/splitit.sh
@@ -6,6 +6,7 @@
 ## Configuration (global) variables go here
 MULTIRUN_PATTERN="PACKAGE"
 TEST_RUN_PREFIX="test_run_"
+THREAD_NUM_FILE="thread_num.tmp"
 DATA_PATTERN="execute"
 
 ## Helper global variables go here
@@ -13,7 +14,7 @@ flag_multirun=""
 
 function is_multirun_log() {
   if [[ `grep -c "$MULTIRUN_PATTERN" $1` -gt 1 ]]; then
-    echo "This log includes multiple runs. Splitting to individual logs..."
+    echo "This log includes multiple runs."
     return 0
   elif  [[ `grep -c "$MULTIRUN_PATTERN" $1` -eq 1 ]]; then
     return 1
@@ -29,17 +30,16 @@ function split_data() {
   rv=$?
   echo "extract_data: rv: $rv"
   if [[ $rv -eq 0 ]]; then
-    echo "extract_data: multirun log found!"
+    echo "extract_data: Splitting to individual logs..."
     flag_multirun=1
     csplit -z -b "%02d.dat" -f "$TEST_RUN_PREFIX" $1 /"$MULTIRUN_PATTERN"/ '{*}'
     echo "Detecting number of threads..."
     ## TODO: Consider moving the data extraction to another script or
     ## rename this file.
-    # TODO: Either use a temp file or a dictionary to store thread numbers.
-    # A temp file should be easier to process e.g. sort by thread number.
+    :>$THREAD_NUM_FILE
     for i in `ls "$TEST_RUN"*.dat`; do
       tnum=`grep -m 1 "$DATA_PATTERN" $i | awk '{print $1}'`
-      echo "$i: $tnum"
+      echo "$i $tnum" >> $THREAD_NUM_FILE
     done
   else
     echo "This is NOT a multirun log"
